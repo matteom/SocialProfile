0ce7f60f6e26bcfb20ae80b4c9a203804f4cdb8d
Matteo Manferdini <matteo@purecreek.com>
2022-08-12 11:30:55 +0200

    Initial Commit

---
+++ SocialProfile/SocialProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProfileApp.swift
+//  SocialProfile
+//
+//  Created by Matteo Manferdini on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
