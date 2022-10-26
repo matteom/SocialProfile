b29ce80889251daf3665c261e29d814734154ac2
Matteo Manferdini <matteo@purecreek.com>
2022-10-26 17:45:19 +0200

    FEAT: Add feature flags

---
+++ SocialProfile/Flags.swift
@@ -0,0 +1,12 @@
+//
+//  Flags.swift
+//  SocialProfile
+//
+//  Created by Matteo Manferdini on 26/10/22.
+//
+
+import Foundation
+
+struct Flags: Codable {
+	var isNavigationEnabled: Bool = false
+}
--- SocialProfile/SocialProfileApp.swift
+++ SocialProfile/SocialProfileApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct SocialProfileApp: App {
+	@Environment(\.scenePhase) private var scenePhase
+	@StateObject private var flagsController = FlagsController()
+
     var body: some Scene {
         WindowGroup {
 			ContentView(user: .preview)
+				.environmentObject(flagsController)
         }
+		.onChange(of: scenePhase) { newScenePhase in
+			if newScenePhase == .active {
+				Task { try? await flagsController.loadFlags() }
+			}
+		}
     }
 }
