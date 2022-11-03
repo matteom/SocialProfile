5dbca75b50081434facc7c4a33888ea9d0ea3772
Matteo Manferdini <matteo@purecreek.com>
2022-11-03 15:37:08 +0100

    REMOVE: Feature flags

--- SocialProfile/SocialProfileApp.swift
+++ SocialProfile/SocialProfileApp.swift
@@ -9,26 +9,11 @@ import SwiftUI
 
 @main
 struct SocialProfileApp: App {
-	@Environment(\.scenePhase) private var scenePhase
-	@StateObject private var flagsController = FlagsController()
-
-    var body: some Scene {
-        WindowGroup {
-			Group {
-				if flagsController.flags.isNavigationEnabled {
-					NavigationStack {
-						ContentView(user: .preview)
-					}
-				} else {
-					ContentView(user: .preview)
-				}
-			}
-			.environmentObject(flagsController)
-        }
-		.onChange(of: scenePhase) { newScenePhase in
-			if newScenePhase == .active {
-				Task { try? await flagsController.loadFlags() }
+	var body: some Scene {
+		WindowGroup {
+			NavigationStack {
+				ContentView(user: .preview)
 			}
 		}
-    }
+	}
 }
